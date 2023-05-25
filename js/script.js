a0a6848a221aa929ebc2be9b70e43bd8a86fd811
hwibaek <hwibaek2827@gmail.com>
2023-05-25 20:45:50 +0900

    first

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function plus(a, b) {
+    return a + b;
+}
+const minus = function (a, b) {
+    return a - b
+}
+const times = (a, b) => a * b;
+const split = (a, b) => a / b;
+
+console.log(plus(10, 2));
+console.log(minus(10, 2));
+console.log(times(10, 2));
+console.log(split(10, 2));
